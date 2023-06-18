26f099d044312cfc8450dd7cd9081960626ee35f
WaqarAhmad707 <officialwaqar707@gmail.com>
2023-06-18 20:11:38 +0500

    App Context fixations

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,13 +1,14 @@
 import { createContext } from "react";
 
+// App Context that holds all the data globally
 export const AppContext = createContext({
-    isLoggedIn: false,
-    selectedPage: null,
-    setSelectedPage: () => {},
-    cars: [],
-    setCars: () => {},
-    categories: [],
-    setCategories: () => {},
-    login: () => {},
-    logOut: () => {}
+  isLoggedIn: false,
+  selectedPage: null,
+  setSelectedPage: () => {},
+  cars: [],
+  setCars: () => {},
+  categories: [],
+  setCategories: () => {},
+  login: () => {},
+  logOut: () => {},
 });
